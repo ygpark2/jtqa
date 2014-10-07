0115030dc53024504c2f7a363f68c695afa8084e
ygpark2 <ygpark2@gmail.com>
2014-10-07 17:07:08 +1000

    del ember validation

--- Brocfile.js
+++ Brocfile.js
@@ -17,4 +17,71 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+// app.import({
+//     development: "bower_components/html5shiv/dist/html5shiv.js",
+//     production: "bower_components/html5shiv/dist/html5shiv-min.js"
+// });
+
+app.import({
+    development: "bower_components/moment/moment.js",
+    production: "bower_components/moment/min/moment.min.js"
+});
+
+// app.import("bower_components/highcharts-release/highcharts-all.js")
+// app.import({
+//     development: "bower_components/d3/d3.js",
+//     production: "bower_components/d3/d3.min.js"
+// });
+
+// app.import({
+//     development: "bower_components/crossfilter/crossfilter.js",
+//     production: "bower_components/crossfilter/crossfilter.min.js"
+// });
+
+// app.import({
+//     development: "bower_components/dc/dc.js",
+//     production: "bower_components/dc/dc.min.js"
+// });
+
+app.import('bower_components/uikit/dist/js/uikit.min.js');
+app.import('bower_components/uikit/dist/js/addons/autocomplete.min.js');
+app.import('bower_components/uikit/dist/js/addons/datepicker.min.js');
+app.import('bower_components/uikit/dist/js/addons/form-password.min.js');
+app.import('bower_components/uikit/dist/js/addons/form-select.min.js');
+app.import('bower_components/uikit/dist/js/addons/htmleditor.min.js');
+app.import('bower_components/uikit/dist/js/addons/nestable.min.js');
+app.import('bower_components/uikit/dist/js/addons/notify.min.js');
+app.import('bower_components/uikit/dist/js/addons/pagination.min.js');
+app.import('bower_components/uikit/dist/js/addons/search.min.js');
+app.import('bower_components/uikit/dist/js/addons/sortable.min.js');
+app.import('bower_components/uikit/dist/js/addons/sticky.min.js');
+app.import('bower_components/uikit/dist/js/addons/timepicker.min.js');
+app.import('bower_components/uikit/dist/js/addons/upload.min.js');
+
+app.import('bower_components/uikit/dist/css/uikit.min.css');
+app.import('bower_components/uikit/dist/css/uikit.gradient.min.css');
+// app.import('bower_components/uikit/dist/css/uikit.almost-flat.min.css');
+
+app.import('bower_components/uikit/dist/css/addons/uikit.addons.min.css');
+app.import('bower_components/uikit/dist/css/addons/uikit.gradient.addons.min.css');
+// app.import('bower_components/uikit/dist/css/addons/uikit.almost-flat.addons.min.css');
+
+app.import('bower_components/uikit/dist/fonts/FontAwesome.otf', {
+    destDir: 'fonts'
+});
+
+app.import('bower_components/uikit/dist/fonts/fontawesome-webfont.eot', {
+    destDir: 'fonts'
+});
+
+app.import('bower_components/uikit/dist/fonts/fontawesome-webfont.ttf', {
+    destDir: 'fonts'
+});
+
+app.import('bower_components/uikit/dist/fonts/fontawesome-webfont.woff', {
+    destDir: 'fonts'
+});
+
+app.import('bower_components/moment/moment.js');
+
 module.exports = app.toTree();
--- config/environment.js
+++ config/environment.js
@@ -12,7 +12,15 @@ module.exports = function(environment) {
         // e.g. 'with-controller': true
       }
     },
-
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self' 'unsafe-inline' 'unsafe-eval' use.typekit.net connect.facebook.net maps.googleapis.com maps.gstatic.com",
+      'font-src': "'self' data: use.typekit.net",
+      'connect-src': "'self' localhost:3000",
+      'img-src': "'self' www.facebook.com p.typekit.net",
+      'style-src': "'self' 'unsafe-inline' use.typekit.net",
+      'frame-src': "s-static.ak.facebook.com static.ak.facebook.com www.facebook.com"
+    },
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
